f0a8c1b739fae1c6945f1a0ac2d294fb384ea3c2
Lauren Kessell <lkessell1@gmail.com>
2021-08-10 11:28:25 -0400

    Add handleIngredientChange function, add value to ingredient buttons

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -14,6 +14,11 @@ class OrderForm extends Component {
     this.setState({ name: e.target.value });
   }
 
+  handleIngredientChange = e => {
+    e.preventDefault();
+    this.setState({ ingredients: [e.target.value, ...this.state.ingredients] })
+  }
+
   handleSubmit = e => {
     e.preventDefault();
     this.clearInputs();
@@ -27,7 +32,7 @@ class OrderForm extends Component {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} value={ingredient} onClick={e => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
       )
