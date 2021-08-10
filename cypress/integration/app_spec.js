e4c7312f1615609f15f6496c1d293a806c4aed71
Lauren Kessell <lkessell1@gmail.com>
2021-08-10 12:46:36 -0400

    Add intercept and stub in beforeEach

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -5,5 +5,19 @@ describe('Testing environment', () => {
 });
 
 describe('App user flows', () => {
-  
+  beforeEach(() => {
+    cy.intercept('http://localhost:3001/api/v1/orders', {
+      orders: [
+        {
+          id: 1,
+          name: Lauren,
+          ingredients: ['carnitas', 'pico de gallo', 'queso fresco']
+        }, {
+          id: 2,
+          name: Lestrende,
+          ingredients: ['steak', 'sour cream']
+        }
+      ]
+    });
+  });
 });
