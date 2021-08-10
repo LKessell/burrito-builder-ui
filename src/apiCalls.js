4fd3a7df71f69ec92a266789f032b409f9eb7269
Lauren Kessell <lkessell1@gmail.com>
2021-08-10 12:05:18 -0400

    Add POST api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,15 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+    .then(response => response.json())
+}
+
+export const postNewOrder = (newOrder) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newOrder)
+  };
+
+  return fetch('http://localhost:3001/api/v1/orders', options)
+    .then(response => response.json())
 }
